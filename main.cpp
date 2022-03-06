bc2aa1d0a35ace98ade4a9077a3405afd689249d
angelinahmbete <99881412+angelinahmbete@users.noreply.github.com>
2022-03-06 11:15:14 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+A County Library Management System
+Created by Agelinah
+on Feb, 2022
+C89 Compiler
+MIT License
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   printf("County library\n");
+    printf("Welcome Ian. \n");
+    printf("What would you like to do?\n");
+    printf("1. Add new user\n");
+    printf("2. Add Book.\n");
+    printf("Selected action: ");
+    return 0;
+}
